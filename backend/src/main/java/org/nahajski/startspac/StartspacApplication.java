8650bedb4afbf8fd69c4fca860476c46ff9a6acf
James Nahajski <james.nahajski+github@gmail.com>
2024-07-23 20:36:38 +0100

    Adding a spring intlzr backend

---
+++ backend/src/main/java/org/nahajski/startspac/StartspacApplication.java
@@ -0,0 +1,13 @@
+package org.nahajski.startspac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartspacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartspacApplication.class, args);
+	}
+
+}
